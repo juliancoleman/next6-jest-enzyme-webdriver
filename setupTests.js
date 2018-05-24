05c3f18d9c181611a8c44c5866cf76556bb4ce4f
Julian Coleman <imago.dei26@gmail.com>
2018-05-24 15:33:58 -0700

    kill driver by pid

--- setupTests.js
+++ setupTests.js
@@ -2,20 +2,22 @@ const { configure } = require("enzyme");
 const Adapter = require("enzyme-adapter-react-16");
 const chromedriver = require("chromedriver");
 const fs = require("fs");
+const childProcess = require("child_process");
 
 require("jsdom-global")();
 
 configure({ adapter: new Adapter() });
 
-// chromedriver.start([
-//     "--url-base=wd/hub",
-//     "--httpPort=9515",
-// ]);
+const driver = chromedriver.start([
+    "--url-base=wd/hub",
+    "--httpPort=9515",
+]);
 
 const screenshotsSaveDir = "./screenshots";
 
 if (!fs.existsSync(screenshotsSaveDir)) {
-    fs.mkdirSync(screenshotsSaveDir);
+  fs.mkdirSync(screenshotsSaveDir);
 }
 
-// chromedriver.stop();
+childProcess.exec(`pkill ${driver.pid}`);
+chromedriver.stop();
